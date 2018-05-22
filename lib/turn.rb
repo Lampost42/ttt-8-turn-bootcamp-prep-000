fd061a217b155f6370a22e5e74d61482cb2158ce
Trevor Russell <truss.42@gmail.com>
2018-05-22 19:23:24 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,37 @@
+def input_to_index(input)
+  index = input.to_i - 1
+end
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return TRUE
+  else
+    return FALSE
+  end
+end
+
+def valid_move?(board, index)
+  if index >= 0 && index <= 8
+    if position_taken?(board, index) == FALSE
+      return TRUE
+    end
+  else
+    return FALSE
+  end
+end
+
+def move(board, index, mark = "X")
+  if valid_move?(board, index) == TRUE
+    board[index] = mark
+  end
+end
+
+
+    
